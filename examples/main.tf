023ff9035cdd3e43fdaa467dc5ff5aaf80365317
Stefan Oehrli <stefan.oehrli@trivadis.com>
2020-10-14 10:35:16 +0200

    update example

--- examples/main.tf
+++ examples/main.tf
@@ -16,7 +16,23 @@
 # Modified...:
 # see git revision history for more information on changes/updates
 # ---------------------------------------------------------------------------
+terraform {
+  required_providers {
+    oci = {
+      source  = "hashicorp/oci"
+      version = "3.96.0"
+    }
+  }
+}
 
+# define the terraform provider
+provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
+}
 
 module "tvdlab-bastion" {
   source  = "Trivadis/tvdlab-bastion/oci"
