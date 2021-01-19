f1e99acfa80e23691f03a20891e54a2e8b0af1e3
Stefan Oehrli <stefan.oehrli@trivadis.com>
2021-01-19 10:16:20 +0100

    change behavior for host file expect content

--- compute.tf
+++ compute.tf
@@ -42,7 +42,7 @@ resource "oci_core_instance" "bastion" {
       guacamole_user            = var.guacamole_user
       guacamole_connections     = base64gzip(local.guacamole_connections)
       authorized_keys           = base64gzip(file(local.ssh_public_key_path))
-      etc_hosts                 = base64gzip(file(local.hosts_file))
+      etc_hosts                 = base64gzip(local.hosts_file)
       fail2ban_config           = base64gzip(templatefile(local.fail2ban_config ,{
         admin_email = var.admin_email
       }))
