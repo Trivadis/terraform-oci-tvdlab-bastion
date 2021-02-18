c98e44e536c8c72c2b32915b59a846387bff18a6
Stefan Oehrli <stefan.oehrli@trivadis.com>
2021-02-18 21:43:49 +0100

    add webhost dns registration

--- dns.tf
+++ dns.tf
@@ -21,10 +21,24 @@ resource "oci_dns_rrset" "bastion" {
   domain          = join(".", [oci_core_instance.bastion.*.hostname_label[count.index], var.tvd_domain])
   rtype           = "A"
   items {
-    domain          = join(".", [oci_core_instance.bastion.*.hostname_label[count.index], var.tvd_domain])
-    rtype           = "A"
-    rdata           = oci_core_instance.bastion.*.public_ip[count.index]
-    ttl             = 30
+    domain = join(".", [oci_core_instance.bastion.*.hostname_label[count.index], var.tvd_domain])
+    rtype  = "A"
+    rdata  = oci_core_instance.bastion.*.public_ip[count.index]
+    ttl    = 30
+  }
+
+}
+
+resource "oci_dns_rrset" "webhost" {
+  count           = var.bastion_enabled == true && var.bastion_dns_registration == true && var.webhost_name != "" ? var.tvd_participants : 0
+  zone_name_or_id = var.tvd_domain
+  domain          = join(".", [var.webhost_name, var.tvd_domain])
+  rtype           = "A"
+  items {
+    domain = join(".", [var.webhost_name, var.tvd_domain])
+    rtype  = "A"
+    rdata  = oci_core_instance.bastion.*.public_ip[0]
+    ttl    = 30
   }
 
 }
