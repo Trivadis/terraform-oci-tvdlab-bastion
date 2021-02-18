e6b2354ea968afd52b933f0e0870d26555ef923f
Stefan Oehrli <stefan.oehrli@trivadis.com>
2021-02-18 21:44:19 +0100

    format output

--- outputs.tf
+++ outputs.tf
@@ -17,31 +17,31 @@
 # display public IPs jumphost
 output "bastion_id" {
   description = "OCID of the bastion server instances."
-  value = oci_core_instance.bastion.*.id
+  value       = oci_core_instance.bastion.*.id
 }
 
 output "bastion_hostname" {
   description = "The hostname for VNIC's primary private IP of the bastion server instances."
-  value = oci_core_instance.bastion.*.hostname_label
+  value       = oci_core_instance.bastion.*.hostname_label
 }
 
 output "bastion_public_ip" {
   description = "The public IP address of the bastion server instances."
-  value = oci_core_instance.bastion.*.public_ip
+  value       = oci_core_instance.bastion.*.public_ip
 }
 
 output "bastion_private_ip" {
   description = "The private IP address of the bastion server instances."
-  value = oci_core_instance.bastion.*.private_ip
+  value       = oci_core_instance.bastion.*.private_ip
 }
 
 output "bastion_dns_records" {
   description = "The DNS records for the bastion server instances."
-  value = [oci_dns_rrset.bastion.*.items]
+  value       = [oci_dns_rrset.bastion.*.items]
 }
 
 output "bastion_ssh_access" {
   description = "SSH access string for bastion hosts."
-  value = formatlist("ssh -A opc@%s.${var.tvd_domain}", oci_core_instance.bastion.*.hostname_label)
+  value       = formatlist("ssh -A opc@%s.${var.tvd_domain}", oci_core_instance.bastion.*.hostname_label)
 }
 # --- EOF -------------------------------------------------------------------
