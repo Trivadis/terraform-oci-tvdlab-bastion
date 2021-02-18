124a5631c3b6e3acac0e56a728a08d700961a54b
Stefan Oehrli <stefan.oehrli@trivadis.com>
2021-02-18 21:44:05 +0100

    move tempfile to locals.tf

--- compute.tf
+++ compute.tf
@@ -36,27 +36,13 @@ resource "oci_core_instance" "bastion" {
   }
 
   metadata = {
-    ssh_authorized_keys = var.ssh_public_key != "" ? var.ssh_public_key : file(var.ssh_public_key_path)
-     user_data = var.bastion_bootstrap != "" ? base64gzip(file(var.bastion_bootstrap)) : base64encode(templatefile("${path.module}/cloudinit/bastion_host.yaml", {
-      yum_upgrade               = var.yum_upgrade
-      guacamole_user            = var.guacamole_user
-      guacamole_connections     = base64gzip(local.guacamole_connections)
-      authorized_keys           = base64gzip(file(local.ssh_public_key_path))
-      etc_hosts                 = base64gzip(local.hosts_file)
-      fail2ban_config           = base64gzip(templatefile(local.fail2ban_config ,{
-        admin_email = var.admin_email
-      }))
-      guacamole_initialization  = base64gzip(templatefile("${path.module}/scripts/guacamole_init.template.sh",{
-        host_name           = var.label_prefix == "none" ? format("${local.resource_shortname}-${var.bastion_name}%02d", count.index) : format("${var.label_prefix}-${local.resource_shortname}-${var.bastion_name}%02d", count.index)
-        domain_name         = var.tvd_domain
-        admin_email         = var.admin_email
-        staging             = var.staging
-        guacamole_enabled   = var.guacamole_enabled
-        guacamole_user      = var.guacamole_user
-        guacadmin_user      = var.guacadmin_user
-        guacadmin_password  = var.guacadmin_password
-      }))
-    })) 
+    ssh_authorized_keys = local.ssh_authorized_keys
+    user_data           = local.bootstrap_cloudinit
+  }
+
+  shape_config {
+    memory_in_gbs = var.bastion_memory_in_gbs
+    ocpus         = var.bastion_ocpus
   }
 
   source_details {
@@ -64,7 +50,7 @@ resource "oci_core_instance" "bastion" {
     source_id               = local.bastion_image_id
     boot_volume_size_in_gbs = var.bastion_boot_volume_size
   }
- 
+
   timeouts {
     create = "60m"
   }
