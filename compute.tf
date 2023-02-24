7ed5ba90504893ecf7b5c1083ccd6150712d9ad5
Stefan Oehrli <stefan.oehrli@accenture.com>
2023-02-24 11:14:59 +0100

    replace public_ssh_port in bootstrap file

--- compute.tf
+++ compute.tf
@@ -41,6 +41,7 @@ resource "oci_core_instance" "bastion" {
     user_data = base64gzip(templatefile(local.bootstrap_cloudinit_template, {
       yum_upgrade           = var.yum_upgrade
       guacamole_user        = var.guacamole_user
+      ssh_port              = var.public_ssh_port
       guacamole_connections = base64gzip(local.guacamole_connections)
       authorized_keys       = base64gzip(local.ssh_authorized_keys)
       etc_hosts             = base64gzip(local.hosts_file)
