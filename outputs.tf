7afe6f0b599ab872f50ed0d51424214db726e41a
Stefan Oehrli <stefan.oehrli@accenture.com>
2023-03-10 11:53:22 +0100

    update tflint issues

--- outputs.tf
+++ outputs.tf
@@ -17,35 +17,35 @@
 # display public IPs jumphost
 output "bastion_id" {
   description = "OCID of the bastion server instances."
-  value       = oci_core_instance.bastion.*.id
+  value       = oci_core_instance.bastion[*].id
 }
 
 output "bastion_hostname" {
   description = "The hostname for VNIC's primary private IP of the bastion server instances."
-  value       = oci_core_instance.bastion.*.hostname_label
+  value       = oci_core_instance.bastion[*].hostname_label
 }
 
 output "bastion_public_ip" {
   description = "The public IP address of the bastion server instances."
-  value       = oci_core_instance.bastion.*.public_ip
+  value       = oci_core_instance.bastion[*].public_ip
 }
 
 output "bastion_private_ip" {
   description = "The private IP address of the bastion server instances."
-  value       = oci_core_instance.bastion.*.private_ip
+  value       = oci_core_instance.bastion[*].private_ip
 }
 
 output "bastion_dns_records" {
   description = "The DNS records for the bastion server instances."
-  value       = [oci_dns_rrset.bastion.*.items]
+  value       = [oci_dns_rrset.bastion[*].items]
 }
 
 output "bastion_ssh_access" {
   description = "SSH access string for bastion hosts with specific port"
-  value       = formatlist("ssh -A -p ${var.inbound_ssh_port} opc@%s.${var.tvd_domain}", oci_core_instance.bastion.*.hostname_label)
+  value       = formatlist("ssh -A -p ${var.inbound_ssh_port} opc@%s.${var.tvd_domain}", oci_core_instance.bastion[*].hostname_label)
 }
 output "bastion_public_url" {
   description = "Bastion Apache Guacammole URL."
-  value       = formatlist("http://%s.${var.tvd_domain}/guacamole", oci_core_instance.bastion.*.hostname_label)
+  value       = formatlist("http://%s.${var.tvd_domain}/guacamole", oci_core_instance.bastion[*].hostname_label)
 }
 # --- EOF -------------------------------------------------------------------
