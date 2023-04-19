7b5937aab4cf51f803870628061d07f29acab0d5
Stefan Oehrli <stefan.oehrli@accenture.com>
2023-04-19 08:02:32 +0200

    update terreform required version to *1.0.0*

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
-# ---------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Trivadis - Part of Accenture, Platform Factory - Data Platforms
 # Saegereistrasse 29, 8152 Glattbrugg, Switzerland
-# ---------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Name.......: main.tf
 # Author.....: Stefan Oehrli (oes) stefan.oehrli@accenture.com
 # Editor.....: Stefan Oehrli
-# Date.......: 2023.03.09
+# Date.......: 2023.04.19
 # Revision...: 
 # Purpose....: Main file for the terraform module tvdlab vcn.
 # Notes......: Define a minimal terraform version
 # Reference..: --
 # License....: Apache License Version 2.0, January 2004 as shown
 #              at http://www.apache.org/licenses/
-# ---------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 terraform {
-  required_version = ">= 0.13.0"
+  required_version = ">= 1.0.0"
 }
-# --- EOF -------------------------------------------------------------------
+# --- EOF ----------------------------------------------------------------------
