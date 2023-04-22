908c1c891520b35a26a6c8184ebfb2dc1493c194
Stefan Oehrli <stefan.oehrli@accenture.com>
2023-04-22 19:55:30 +0200

    change bootstrap status file to /etc/boostrap_config_status

--- scripts/guacamole_init.template.sh
+++ scripts/guacamole_init.template.sh
@@ -54,7 +54,7 @@ exec 2>&1                       # Redirect standard error to standard out
 # - Main -----------------------------------------------------------------------
 echo "INFO: Start to initialize the guacamole stack at $(date)" 
 # set the bootstrap config status to running
-echo "running" >/etc/boostrap_config_status
+echo "running" >/var/log/boostrap_custom_config_status
 # get latest release of docker-compose
 if ! command -v docker-compose &> /dev/null; then
     DOCKER_COMPOSE_URL=$(curl -sL https://api.github.com/repos/docker/compose/releases/latest | jq -r '.assets[].browser_download_url'|grep -iE "$(uname -s)-$(uname -m)$")
@@ -87,7 +87,7 @@ if [ ! -d "/home/$GUACAMOLE_USER/guacamole" ]; then
 else
     echo "ERR : /home/$GUACAMOLE_USER/guacamole already exists ..."
     # set the bootstrap config status to error
-    echo "error" >/etc/boostrap_config_status
+    echo "error" >/var/log/boostrap_custom_config_status
     exit 1
 fi
 
@@ -159,7 +159,7 @@ export PRIVATE_IP=\$(hostname -I |cut -d' ' -f1)
 
 # cloud-init status information
 BOOTSTRAP_STATUS1=\$((sudo cloud-init status 2>/dev/null|| echo "n/a")|cut -d' ' -f2|sed 's/ //g')
-BOOTSTRAP_STATUS2=\$(cat /etc/boostrap_config_status 2>/dev/null|| echo "n/a")
+BOOTSTRAP_STATUS2=\$(cat /var/log/boostrap_custom_config_status 2>/dev/null|| echo "n/a")
 
 echo "
 ===============================================================================
@@ -189,9 +189,9 @@ rm -vf /etc/bash_completion.d/docker-compose
 
 # check if we do have an error in the log file
 if [ $(grep -iE 'error|err' $LOGFILE|wc -l) -ne 0 ]; then 
-    echo "error" >/etc/boostrap_config_status
+    echo "error" >/var/log/boostrap_custom_config_status
 else
-    echo "done" >/etc/boostrap_config_status
+    echo "done" >/var/log/boostrap_custom_config_status
 fi
 
 # set the bootstrap config status to done
